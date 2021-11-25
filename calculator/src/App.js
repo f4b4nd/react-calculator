ac679575652ee8b414c073248d1ea4553d485c54
fabien <fabien.andrian@gmail.com>
2021-11-25 13:12:16 +0100

    first reducer

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,37 +1,58 @@
+import { useReducer } from 'react'
+
 import DigitButton from './components/DigitButton'
 import OperationButton from './components/OperationButton'
-import './App.scss';
 
+import './App.scss'
 
-function App() {
-  return (
-    <div className="App">
-      <div class="calculator">
-        <div className="output">
-          <div className="current-operand"> 123*</div>
-          <div className="previous-operand"> 3</div>
+function init (initialOperand) {
+    return {
+        currentOperand: initialOperand,
+        previousOperand: initialOperand,
+    }
+}
+
+function reducer (state, {type, payload}) {
+    switch (type) {
+        case 'digit':
+            return {
+                ...state, 
+                currentOperand: state.currentOperand !== null ? state.currentOperand.toString() + payload.toString() : payload.toString()
+            }
+    }
+}
+
+function App () {
+    const [state, dispatch] = useReducer(reducer, null, init)
+    return (
+        <div className="App">
+            <div className="calculator">
+                <div className="output">
+                    <div className="previous-operand"> {state.previousOperand} </div>                    
+                    <div className="current-operand"> {state.currentOperand}</div>
+
+                </div>
+                <button className="ac"> AC </button>
+                <button className="del"> DEL </button>
+                <OperationButton operator={"÷"} /> 
+                <DigitButton digit={1} dispatch={dispatch} /> 
+                <DigitButton digit={2} dispatch={dispatch} />
+                <DigitButton digit={3} dispatch={dispatch} /> 
+                <OperationButton operator={"*"} /> 
+                <DigitButton digit={4} dispatch={dispatch} /> 
+                <DigitButton digit={5} dispatch={dispatch} /> 
+                <DigitButton digit={6} dispatch={dispatch} />
+                <OperationButton operator={"+"} />
+                <DigitButton digit={7} dispatch={dispatch} /> 
+                <DigitButton digit={8} dispatch={dispatch} /> 
+                <DigitButton digit={9} dispatch={dispatch} />
+                <OperationButton operator={"-"} />
+                <DigitButton digit={'.'} dispatch={dispatch} /> 
+                <DigitButton digit={0} dispatch={dispatch} /> 
+                <button className="equals"> = </button>
+            </div>
         </div>
-        <button className="ac"> AC </button>
-        <button className="del"> DEL </button>
-        <OperationButton operator={"÷"} /> 
-        <DigitButton digit={1} /> 
-        <DigitButton digit={2} />
-        <DigitButton digit={3} /> 
-        <OperationButton operator={"*"} /> 
-        <DigitButton digit={4} /> 
-        <DigitButton digit={5} /> 
-        <DigitButton digit={6} />
-        <OperationButton operator={"+"} />
-        <DigitButton digit={7} /> 
-        <DigitButton digit={8} /> 
-        <DigitButton digit={9} />
-        <OperationButton operator={"-"} />
-        <DigitButton digit={'.'} /> 
-        <DigitButton digit={0} /> 
-        <button className="equals"> = </button>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default App
--- calculator/src/components/DigitButton.js
+++ calculator/src/components/DigitButton.js
@@ -1,5 +1,8 @@
-const DigitButton = (props) => (
-    <button> {props.digit} </button>
+
+const DigitButton = ( {digit, dispatch }) => (
+    <button onClick={() => dispatch({type: 'digit', payload: digit})}> 
+        {digit} 
+    </button>
 )
 
 export default DigitButton 
